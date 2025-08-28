1975f889dc036b6a565f323ccb4a58c2ecda7402
3202910 <makoto_fukumoto@mail.toyota.co.jp>
2025-08-28 10:56:02 +0900

    BMC3

--- openpilot_onnx.py
+++ openpilot_onnx.py
@@ -7,7 +7,6 @@ import pandas as pd
 from matplotlib import pyplot as plt
 
 #今から改造開始_20250828_fuku
-#確認コメント追加_2050828_fuku
 
 X_IDXS = np.array([ 0. ,   0.1875,   0.75  ,   1.6875,   3.    ,   4.6875,
          6.75  ,   9.1875,  12.    ,  15.1875,  18.75  ,  22.6875,
